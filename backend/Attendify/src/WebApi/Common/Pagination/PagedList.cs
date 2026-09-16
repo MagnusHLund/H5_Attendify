6ba85431cf43cdc860f28608d3c277a2defd1176
Jorjo67 <kiro.nicolov@gmail.com>
2026-09-16 15:50:01 +0200

    Added missing pagination capability from template

---
+++ backend/Attendify/src/WebApi/Common/Pagination/PagedList.cs
@@ -0,0 +1,10 @@
+namespace Attendify.Common.Pagination;
+
+public sealed record PagedList<T>(IReadOnlyList<T> Items, int Page, int PageSize, int TotalCount)
+{
+    public int TotalPages => PageSize <= 0 ? 0 : (int)Math.Ceiling(TotalCount / (double)PageSize);
+
+    public bool HasPreviousPage => Page > 1;
+
+    public bool HasNextPage => Page < TotalPages;
+}
