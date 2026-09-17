667c79589013202a469135ae13bf25e349508e0f
Magnus Lund <magnusherringelund@gmail.com>
2026-09-17 14:05:09 +0200

    Added "Plus Jakarta Sans" font

--- webapp/Attendify/vite.config.ts
+++ webapp/Attendify/vite.config.ts
@@ -24,7 +24,10 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@use "@/assets/styles/variables.scss" as *;`,
+        additionalData: `
+          @use "@/assets/styles/variables.scss" as *; 
+          @use "@/assets/styles/fonts.scss" as *;
+        `,
       },
     },
   },
