2c2d9dd6fd6f01b38f7d97f116fbdcc5df21437e
Jorjo67 <kiro.nicolov@gmail.com>
2026-09-21 16:27:17 +0200

    feat: Added changes that allow for kubernetes deployment through aspire deploy

--- backend/Attendify/tools/AppHost/AppHost.cs
+++ backend/Attendify/tools/AppHost/AppHost.cs
@@ -1,12 +1,11 @@
+using Aspire.Hosting.Yarp.Transforms;
 using Projects;
+
+#pragma warning disable ASPIRECOMPUTE003
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var tunnelName = builder.Configuration.GetSection("Parameters")["CloudflareTunnelName"];
-if (string.IsNullOrWhiteSpace(tunnelName))
-    throw new InvalidOperationException("CloudflareTunnelName is required.");
-
-var tunnel = builder.AddCloudflareTunnel(tunnelName);
+var tunnel = builder.AddCloudflareTunnel("lunnel");
 
 var hostname = builder.Configuration.GetSection("Parameters")["Hostname"];
 if (string.IsNullOrWhiteSpace(hostname))
@@ -18,8 +17,19 @@ var facialEmbeddingEncryptionKey = builder.AddParameter(
     secret: true
 );
 
+var endpoint = builder.AddParameter("registry-endpoint");
+var repository = builder.AddParameter("registry-repository");
+var registry = builder.AddContainerRegistry("registry", endpoint, repository);
+var postgresPassword = builder.AddParameter(
+    "postgres-password",
+    secret: true);
+
+var k8s = builder.AddKubernetesEnvironment("k8s")
+    .WithContainerRegistry(registry);
+
 var postgres = builder
-    .AddPostgres("postgres")
+    .AddPostgres("postgres",
+        password: postgresPassword)
     .WithDataVolume()
     .WithLifetime(ContainerLifetime.Persistent);
 
@@ -27,11 +37,13 @@ var db = postgres.AddDatabase("AppDb", "app-db");
 
 var migrationService = builder
     .AddProject<MigrationService>("migrations")
+    .WithContainerRegistry(registry)
     .WithReference(db)
     .WaitFor(postgres);
 
 var api = builder
     .AddProject<WebApi>("api")
+    .WithContainerRegistry(registry)
     .WithExternalHttpEndpoints()
     .WithReference(db)
     .WithEnvironment("Jwt__SigningKey", jwtSigningKey)
@@ -41,16 +53,17 @@ var api = builder
 var webapp = builder
     .AddViteApp("webapp", "../../../../webapp/Attendify")
     .WithReference(api)
+    .WithContainerRegistry(registry)
     .WaitFor(api)
     .WithExternalHttpEndpoints();
 
-var gateway = builder
-    .AddYarp("gateway")
+var gateway = builder.AddYarp("gateway")
+    .WithHttpEndpoint(port: 80, targetPort: 5000, name: "http")
     .WithConfiguration(yarp =>
     {
         yarp.AddRoute("/api/{**catch-all}", api);
         yarp.AddRoute("{**catch-all}", webapp);
-    });
+    }).PublishWithStaticFiles(webapp);
 
 gateway.WithCloudflareTunnel(tunnel, hostname: hostname);
 
