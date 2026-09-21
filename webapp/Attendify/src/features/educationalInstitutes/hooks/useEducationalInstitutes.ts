dd36f5d9b721ab254a2567b4adcd9f770736dea3
Magnus Lund <magnusherringelund@gmail.com>
2026-09-21 16:35:48 +0200

    Changed css for centering spinner, to apply to more browsers.
    Added an hours memory cache of the educational institutes.

--- webapp/Attendify/src/features/educationalInstitutes/hooks/useEducationalInstitutes.ts
+++ webapp/Attendify/src/features/educationalInstitutes/hooks/useEducationalInstitutes.ts
@@ -5,6 +5,7 @@ export function useEducationalInstitutes(enabled = true) {
   return useQuery({
     queryKey: ['educational-institutes'],
     queryFn: getEducationalInstitutes,
+    staleTime: 60 * 60 * 1000, // 60 minutes
     retry: false,
     enabled,
   })
