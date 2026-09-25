92faa4ef3b49d0e1008cf2694bd1b914e50cc6b2
Magnus Lund <magnusherringelund@gmail.com>
2026-09-25 14:11:50 +0200

    Modified math, to be a ceiling calculation

--- backend/Attendify/src/WebApi/Features/Settings/UpdateFacePictures/UpdateFacePicturesValidator.cs
+++ backend/Attendify/src/WebApi/Features/Settings/UpdateFacePictures/UpdateFacePicturesValidator.cs
@@ -2,20 +2,14 @@ namespace Attendify.Features.Settings.UpdateFacePictures;
 
 public sealed class UpdateFacePicturesValidator : Validator<UpdateFacePicturesRequest>
 {
-    private const int MaxBase64PhotoLength = 5 * 1024 * 1024 / 3 * 4;
+    private const int MaxBase64PhotoLength = ((5 * 1024 * 1024 + 2) / 3) * 4;
 
     public UpdateFacePicturesValidator()
     {
-        RuleFor(request => request.StraightPhoto)
-            .NotEmpty()
-            .MaximumLength(MaxBase64PhotoLength);
+        RuleFor(request => request.StraightPhoto).NotEmpty().MaximumLength(MaxBase64PhotoLength);
 
-        RuleFor(request => request.LeftPhoto)
-            .NotEmpty()
-            .MaximumLength(MaxBase64PhotoLength);
+        RuleFor(request => request.LeftPhoto).NotEmpty().MaximumLength(MaxBase64PhotoLength);
 
-        RuleFor(request => request.RightPhoto)
-            .NotEmpty()
-            .MaximumLength(MaxBase64PhotoLength);
+        RuleFor(request => request.RightPhoto).NotEmpty().MaximumLength(MaxBase64PhotoLength);
     }
 }
